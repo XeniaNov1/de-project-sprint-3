8a79ca18fc20f89a8d566611b8a8eac43fd816e0
XeniaNov1 <novikova.xn@yandex.ru>
2024-10-28 23:33:26 +0300

    add logging

--- dags/sprint3.py
+++ dags/sprint3.py
@@ -2,6 +2,7 @@ import time
 import requests
 import json
 import pandas as pd
+import logging
 
 from datetime import datetime, timedelta
 from airflow import DAG
@@ -13,42 +14,55 @@ from airflow.providers.http.hooks.http import HttpHook
 http_conn_id = HttpHook.get_connection('http_conn_id')
 api_key = http_conn_id.extra_dejson.get('api_key')
 base_url = http_conn_id.host
+logger = logging.getLogger(__name__)
 
-postgres_conn_id = 'postgresql_de'
+POSTGRESS_CONN_ID = 'postgresql_de'
 
-nickname = 'novikova-xn'
-cohort = '30'
+NICKNAME = 'novikova-xn'
+COHORT = '30'
 
-headers = {
-    'X-Nickname': nickname,
-    'X-Cohort': cohort,
+COMMON_HEADERS = {
+    'X-Nickname': NICKNAME,
+    'X-Cohort': COHORT,
     'X-Project': 'True',
     'X-API-KEY': api_key,
     'Content-Type': 'application/x-www-form-urlencoded'
 }
 
+DEFAULT_DAG_ARGS = {
+    "owner": NICKNAME,
+    'email': ['novikova-xn@yandex.ru'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 0
+}
+
+BUSINESS_DT = '{{ ds }}'
+
 
 def generate_report(ti):
-    print('Making request generate_report')
+    logger.info('Making request generate_report')
 
-    response = requests.post(f'{base_url}/generate_report', headers=headers)
+    response = requests.post(
+        f'{base_url}/generate_report',
+        headers=COMMON_HEADERS)
     response.raise_for_status()
     task_id = json.loads(response.content)['task_id']
     ti.xcom_push(key='task_id', value=task_id)
-    print(f'Response is {response.content}')
+    logger.info(f'Response is {response.content}')
 
 
 def get_report(ti):
-    print('Making request get_report')
+    logger.info('Making request get_report')
     task_id = ti.xcom_pull(key='task_id')
 
     report_id = None
 
     for i in range(20):
         response = requests.get(
-            f'{base_url}/get_report?task_id={task_id}', headers=headers)
+            f'{base_url}/get_report?task_id={task_id}', headers=COMMON_HEADERS)
         response.raise_for_status()
-        print(f'Response is {response.content}')
+        logger.debug(f'Response is {response.content}')
         status = json.loads(response.content)['status']
         if status == 'SUCCESS':
             report_id = json.loads(response.content)['data']['report_id']
@@ -60,18 +74,17 @@ def get_report(ti):
         raise TimeoutError()
 
     ti.xcom_push(key='report_id', value=report_id)
-    print(f'Report_id={report_id}')
+    logger.info(f'Report_id={report_id}')
 
 
 def get_increment(date, ti):
-    print('Making request get_increment')
+    logger.info('Making request get_increment')
     report_id = ti.xcom_pull(key='report_id')
     response = requests.get(
-        f'{base_url}/get_increment?report_id={
-            report_id}&date={str(date)}T00:00:00',
-        headers=headers)
+        f'{base_url}/get_increment?report_id={report_id}&date={str(date)}T00:00:00',
+        headers=COMMON_HEADERS)
     response.raise_for_status()
-    print(f'Response is {response.content}')
+    logger.debug(f'Response is {response.content}')
 
     increment_id = json.loads(response.content)['data']['increment_id']
     if not increment_id:
@@ -79,20 +92,20 @@ def get_increment(date, ti):
             'Increment is empty. Most probably due to error in API call.')
 
     ti.xcom_push(key='increment_id', value=increment_id)
-    print(f'increment_id={increment_id}')
+    logger.info(f'increment_id={increment_id}')
 
 
 def upload_data_to_staging(filename, date, pg_table, pg_schema, ti):
     increment_id = ti.xcom_pull(key='increment_id')
-    s3_filename = f'https://storage.yandexcloud.net/s3-sprint3/cohort_{
-        cohort}/{nickname}/project/{increment_id}/{filename}'
-    print(s3_filename)
+    s3_filename = f'https://storage.yandexcloud.net/s3-sprint3/cohort_{COHORT}/{NICKNAME}/project/{increment_id}/{filename}'
+    logger.debug(s3_filename)
     local_filename = date.replace('-', '') + '_' + filename
-    print(local_filename)
+    logger.info(local_filename)
     response = requests.get(s3_filename)
     response.raise_for_status()
-    open(f"{local_filename}", "wb").write(response.content)
-    print(response.content)
+    with open(f"{local_filename}", "wb") as f:
+        f.write(response.content)
+    logger.debug(response.content)
 
     df = pd.read_csv(local_filename)
     df = df.drop('id', axis=1)
@@ -101,26 +114,16 @@ def upload_data_to_staging(filename, date, pg_table, pg_schema, ti):
     if 'status' not in df.columns:
         df['status'] = 'shipped'
 
-    postgres_hook = PostgresHook(postgres_conn_id)
+    postgres_hook = PostgresHook(POSTGRESS_CONN_ID)
     engine = postgres_hook.get_sqlalchemy_engine()
     row_count = df.to_sql(pg_table, engine, schema=pg_schema,
                           if_exists='append', index=False)
-    print(f'{row_count} rows was inserted')
+    logger.info(f'{row_count} rows was inserted')
 
-
-args = {
-    "owner": nickname,
-    'email': ['novikova-xn@yandex.ru'],
-    'email_on_failure': False,
-    'email_on_retry': False,
-    'retries': 0
-}
-
-business_dt = '{{ ds }}'
 
 with DAG(
         'sales_mart',
-        default_args=args,
+        default_args=DEFAULT_DAG_ARGS,
         description='Provide default dag for sprint3',
         catchup=True,
         start_date=datetime.today() - timedelta(days=7),
@@ -137,42 +140,42 @@ with DAG(
     get_increment = PythonOperator(
         task_id='get_increment',
         python_callable=get_increment,
-        op_kwargs={'date': business_dt})
+        op_kwargs={'date': BUSINESS_DT})
 
     upload_user_order_inc = PythonOperator(
         task_id='upload_user_order_inc',
         python_callable=upload_data_to_staging,
-        op_kwargs={'date': business_dt,
+        op_kwargs={'date': BUSINESS_DT,
                    'filename': 'user_order_log_inc.csv',
                    'pg_table': 'user_order_log',
                    'pg_schema': 'staging'})
 
     update_d_item_table = PostgresOperator(
         task_id='update_d_item',
-        postgres_conn_id=postgres_conn_id,
+        postgres_conn_id=POSTGRESS_CONN_ID,
         sql="sql/mart.d_item.sql")
 
     update_d_customer_table = PostgresOperator(
         task_id='update_d_customer',
-        postgres_conn_id=postgres_conn_id,
+        postgres_conn_id=POSTGRESS_CONN_ID,
         sql="sql/mart.d_customer.sql")
 
     update_d_city_table = PostgresOperator(
         task_id='update_d_city',
-        postgres_conn_id=postgres_conn_id,
+        postgres_conn_id=POSTGRESS_CONN_ID,
         sql="sql/mart.d_city.sql")
 
     update_f_sales = PostgresOperator(
         task_id='update_f_sales',
-        postgres_conn_id=postgres_conn_id,
+        postgres_conn_id=POSTGRESS_CONN_ID,
         sql="sql/mart.f_sales.sql",
-        parameters={"date": {business_dt}}
+        parameters={"date": {BUSINESS_DT}}
     )
     insert_f_customer_retention = PostgresOperator(
         task_id='insert_f_customer_retention',
-        postgres_conn_id=postgres_conn_id,
+        postgres_conn_id=POSTGRESS_CONN_ID,
         sql="sql/mart.f_customer_retention.sql",
-        parameters={"date": {business_dt}}
+        parameters={"date": {BUSINESS_DT}}
     )
 
     (
--- project/run_migrations.py
+++ project/run_migrations.py
@@ -1,6 +1,10 @@
 import psycopg2
 import pandas as pd
 import numpy as np
+import logging
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.DEBUG, format='[%(asctime)s] %(levelname)s %(message)s')
 
 conn = psycopg2.connect(
     "host='localhost' port='5432' dbname='de' user='jovyan' password='jovyan'")
@@ -8,24 +12,25 @@ cur = conn.cursor()
 
 with open('project/migrations/01-uol.sql', 'r') as uol01:
     uol_data = uol01.read()
-
+    logger.debug(f'The file is read:\n{uol_data}')
     cur.execute(uol_data)
     conn.commit()
+    logger.info('First transaction was commited')
 
 
-with open('project/migrations/02-f_sales.sql', 'r') as fsales02:
-    f_sales_data = fsales02.read()
-
-    cur.execute(f_sales_data)
-    conn.commit()
-
-    fsales02.close()
+# with open('project/migrations/02-f_sales.sql', 'r') as fsales02:
+#     f_sales_data = fsales02.read()
+#     logger.debug(f'The file is read:\n{f_sales_data}')
+#     cur.execute(f_sales_data)
+#     conn.commit()
+#     logger.info('Second transaction was commited')
 
 with open('project/migrations/03-create-mart.f_customer_retention.sql', 'r') as custret03:
     ret_data = custret03.read()
-
+    logger.debug(f'The file is read:\n{ret_data}')
     cur.execute(ret_data)
     conn.commit()
+    logger.info('Third transaction was commited')
 
 cur.close()
 conn.close()
